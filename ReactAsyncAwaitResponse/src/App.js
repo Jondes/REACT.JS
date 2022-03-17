e4e153f389a36f0186508ad518c2723377147d31
Johnson E Guedes <jondes560@hotmail.com>
2022-03-17 10:08:05 +0000

    async-await-http-response
    React-async_await_http

---
+++ ReactAsyncAwaitResponse/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Country from './components/Country'
+
+export default function App() {
+  return (
+    <div className="container mt-5">
+      <Country />
+    </div>
+  )
+}
