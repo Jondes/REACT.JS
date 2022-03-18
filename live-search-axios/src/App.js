6a1255d397a52fc270b594962a96d0f21e49f1c5
Johnson E Guedes <jondes560@hotmail.com>
2022-03-18 06:14:38 +0000

    Live search axios jsonplaceholder
    Live search axios jsonplaceholder

---
+++ live-search-axios/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import LiveSearchFilter from './components/LiveSearchFilter.js'
+function App() {
+  return (
+    <div className='container mt-3'>
+      <LiveSearchFilter />
+    </div>
+  )
+}
+export default App;
