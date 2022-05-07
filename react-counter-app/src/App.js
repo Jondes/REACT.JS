d4921c23803b5ddb07f941fd89f8807592074950
Johnson E Guedes <jondes560@hotmail.com>
2022-05-07 13:21:18 +0100

    React Counter
    React Counter App

---
+++ react-counter-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactCounter from './components/ReactCounter'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="container mt-3">
+      <ReactCounter />
+    </div>
+  )
+}
+export default App
