94fa987cce5905ac45766f213ed959c3856607f9
Johnson E Guedes <jondes560@hotmail.com>
2022-05-14 21:38:58 +0100

    React Tabs
    React Tabs

---
+++ react-tabs/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactTabs from './components/ReactTabs'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="container mt-3">
+      <ReactTabs />
+    </div>
+  )
+}
+export default App
