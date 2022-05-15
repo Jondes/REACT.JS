17d09b102dfdc768184f8dd859480570b9cbde91
Johnson E Guedes <jondes560@hotmail.com>
2022-05-15 18:23:04 +0100

    React Show Hide
    React Show Hide

---
+++ react-panel-show-hide/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import HideShow from './components/HideShow'
+function App() {
+  return (
+    <div className="container mt-5">
+      <HideShow />
+    </div>
+  )
+}
+export default App
