dfb0cc92ea452aca98cf4a2479e818964fccd1a6
Johnson E Guedes <jondes560@hotmail.com>
2022-06-29 19:55:48 +0100

    Installed React Router Dom v6

--- React-MovieApp/movie-app/src/App.js
+++ React-MovieApp/movie-app/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import Home from './Home.js'
+import Home from './Home.js';
+import SingleMovie from './SingleMovie.js';
+import Error from './Error.js';
+
+import { Routes,Route } from "react-router-dom";
+
 
 const App = () => {
   return (
-    <div><Home /></div>
+    <>
+    
+     <Routes>
+      <Route path='/' element={<Home /> }/>
+      <Route path='movie/:id' element={<SingleMovie /> }/>
+      <Route path='*' element={ <Error />} />    
+    </Routes>
+    
+    </>
   )
 }
 
