fdf67d49cd7778757c1e87cad395443e14b7da92
Johnson E Guedes <8356741+Jondes@users.noreply.github.com>
2022-08-24 14:28:10 +0100

    Initial commit

---
+++ react-responsive-carousel/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CarouselComponent from './components/carousel.component';
+
+function App() {
+  return (
+    <div className="App">
+      <CarouselComponent/>
+    </div>
+  );
+}
+
+export default App;
