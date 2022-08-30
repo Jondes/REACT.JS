50737f7d6951ac277eb94319ad1cb138c849e130
Johnson E Guedes <8356741+Jondes@users.noreply.github.com>
2022-08-30 14:26:53 +0100

    Created Components

---
+++ ReactRouterDom/reactrouter/src/component/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+  
+function Contact (){
+ return <address>
+            You can find me here:<br /><br/>
+            Johnson Guedes<br />
+            Shepherds Bush, <br />
+            London (W12)
+        </address>
+}
+  
+export default Contact;
