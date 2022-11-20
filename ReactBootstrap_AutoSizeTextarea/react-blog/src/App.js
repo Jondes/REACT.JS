ea0123a06b0f1b4b4c8788b46a7ba432b0b3afaf
Johnson Guedes <jondes560@hotmail.com>
2022-11-20 13:32:29 +0000

    Created app

---
+++ ReactBootstrap_AutoSizeTextarea/react-blog/src/App.js
@@ -0,0 +1,11 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Home from "./components/Home";
+function App() {
+  return (
+    <div className="container mt-3">
+      <Home />
+    </div>
+  );
+}
+export default App;
