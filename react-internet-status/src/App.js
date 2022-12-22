d2134c0aa1ef4f6fb2f7f8017cab91003bd3440a
Johnson Guedes <jondes560@hotmail.com>
2022-12-22 20:40:04 +0000

    App commit

---
+++ react-internet-status/src/App.js
@@ -0,0 +1,11 @@
+import Network from "./components/Network";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+function App() {
+  return (
+    <div className="container">
+      <Network />
+    </div>
+  );
+}
+export default App;
