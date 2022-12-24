cd1569e75aa785cb11cf5ca1af947e1d620b07d2
Johnson Guedes <jondes560@hotmail.com>
2022-12-24 14:42:04 +0000

    Index commit

---
+++ react-internet-status/src/components/Network.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import connected from "../img/connected.jpg";
+import networkLost from "../img/connection-lost.jpg";
+function Network() {
+  const [status, setStatus] = useState(() => {
+    if (navigator.onLine) {
+      console.log("Connected to network.");
+      return true;
+    } else {
+      return false;
+    }
+  });
+  useEffect(() => {
+    window.ononline = (e) => {
+      console.log("Connected to network.");
+      setStatus(true);
+    };
+    window.onoffline = (e) => {
+      console.log("Network connection lost.");
+      setStatus(false);
+    };
+  }, [status]);
+  return (
+    <div>
+      <h2 className="mb-3">React Detect Network Connection Status Example</h2>
+      {status ? (
+        <>
+          <div className="alert alert-success mb-3">
+            Network is fullly connected
+          </div>
+          <img src={connected} width={420} alt="Logo" />
+        </>
+      ) : (
+        <img src={networkLost} width={420} alt="Logo" />
+      )}
+    </div>
+  );
+}
+export default Network;
