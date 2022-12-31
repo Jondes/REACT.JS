f7509f508e0076b811c634bb94b3184efce6a107
Johnson Guedes <jondes560@hotmail.com>
2022-12-31 20:18:20 +0000

    Speed commit

---
+++ react-internet/src/components/ConnectionSpeed.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../../node_modules/react-internet-meter/dist/index.css";
+import { ReactInternetSpeedMeter } from "react-internet-meter";
+function ConnectionSpeed() {
+  const [checkSpeed, SetCheckSpeed] = React.useState(
+    "Finding internet speed."
+  );
+  return (
+    <div>
+      <h2 className="mb-3">React Find Internet Speed Example</h2>
+      <ReactInternetSpeedMeter
+        txtSubHeading="Internet connection is slow"
+        outputType="" // "alert"/"modal"/"empty"
+        customClassName={null}
+        pingInterval={2000} // milliseconds
+        txtMainHeading="Opps..."
+        thresholdUnit="megabyte" // "byte" , "kilobyte", "megabyte"
+        threshold={50}
+        imageUrl="https://i.postimg.cc/sft772VP/speedometer.png"
+        downloadSize="1561257" //bytes
+        callbackFunctionOnNetworkDown={(data) =>
+          console.log(`Internet speed : ${data}`)
+        }
+        callbackFunctionOnNetworkTest={(data) => SetCheckSpeed(data)}
+      />
+      <div className="card-body mt-4">
+        <span className="display-1">{checkSpeed} MB/s</span>
+      </div>
+    </div>
+  );
+}
+export default ConnectionSpeed;
