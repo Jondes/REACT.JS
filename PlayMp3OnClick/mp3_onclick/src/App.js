51982e60712effe389469551019e66c6a3539b7f
Johnson Guedes <jondes560@hotmail.com>
2023-10-05 07:45:28 +0100

    PlayMp3OnClick commit

---
+++ PlayMp3OnClick/mp3_onclick/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import UserProfile from "./components/UserProfile";
+function App() {
+  return (
+    <div className="container mt-3">
+      <h2 className="mb-3">React Play Mp3 Audio File Example</h2>
+      <UserProfile />
+    </div>
+  );
+}
+export default App;
