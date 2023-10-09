8725526a4d4ad31e6c061a1b9bdf0681342a42dd
Johnson Guedes <jondes560@hotmail.com>
2023-10-09 06:35:54 +0100

    Dynamic data initial commit

---
+++ React-DynamicData/dynamicdata/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Users from './components/Users.js'
+function App() {
+  return (
+    <div>
+      <Users />
+    </div>
+  )
+}
+export default App;
