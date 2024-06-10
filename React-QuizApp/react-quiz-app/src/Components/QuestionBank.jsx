5e42524574f80e358d6120ebc54988e860546c4e
Johnson Guedes <jondes560@hotmail.com>
2024-06-10 16:38:08 +0100

    React Quiz App questionBank

---
+++ React-QuizApp/react-quiz-app/src/Components/QuestionBank.jsx
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+	{
+		id: 1,
+		question: "What is the capital of France?",
+		options: ["Lyon", "Nice", "Paris", "Bordeaux"],
+		answer: "Paris",
+	},
+	{
+		id: 2,
+		question: "What is the capital the UK?",
+		options: ["Manchester", "London", "Birmingham", "Liverpool"],
+		answer: "London",
+	},
+	{
+		id: 3,
+		question: "What is the capital of Spain?",
+		options: ["Barcelona", "Madrid", "Seville", "Valencia"],
+		answer: "Madrid"
+	},
+	{
+		id: 4,
+		question: "What is the capital of Italy?",
+		options: ["Rome", "Milan", "Venice", "Turin"],
+		answer: "Rome"
+	},
+	{
+		id: 5,
+		question: "What is capital of USA?",
+		options: ["New York", "Washington,D.C.", "San Francisco", "Chicago"],
+		answer: "Washington,D.C"
+	},
+]
+
+export default qBank;
