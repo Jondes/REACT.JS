2a67a1340ddc7a5479ce2bd16c2d93a2ec11c0c8
Johnson Guedes <jondes560@hotmail.com>
2024-06-16 09:32:49 +0100

    React Quiz App Website.jsx

---
+++ React-QuizApp/react-quiz-app/src/Components/Website.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Website = () => {
+  return (
+    <div>
+      <div className="WebPage" style={{width: 1024, height: 1366, position: 'relative', background: '#F5F6F6'}}>
+  <div className="IcOutlineArrowDropDown" style={{width: 40, height: 40, paddingLeft: 11.67, paddingRight: 11.67, left: 658, top: 51, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+    <div className="Vector" style={{width: 16.67, height: 8.33, background: '#545454'}}></div>
+  </div>
+  <div className="Outer" style={{width: 300, height: 400, left: 29, top: 358, position: 'absolute', background: '#FFFDFD', borderRadius: 15}} />
+  <img className="Inner" style={{width: 280, height: 200, left: 41, top: 394, position: 'absolute', borderRadius: 15}} src="https://via.placeholder.com/280x200" />
+  <div className="Outer" style={{width: 300, height: 400, left: 359, top: 358, position: 'absolute', background: '#FFFDFD', borderRadius: 15}} />
+  <img className="Inner" style={{width: 200, height: 200, left: 412, top: 394, position: 'absolute', borderRadius: 15}} src="https://via.placeholder.com/200x200" />
+  <div className="Outer" style={{width: 300, height: 400, left: 689, top: 358, position: 'absolute', background: '#FFFDFD', borderRadius: 15}} />
+  <img className="Inner" style={{width: 200, height: 200, left: 739, top: 385, position: 'absolute', borderRadius: 15}} src="https://via.placeholder.com/200x200" />
+  <img className="Rectangle80" style={{width: 75, height: 75, left: 59, top: 38, position: 'absolute'}} src="https://via.placeholder.com/75x75" />
+  <div className="Latest" style={{left: 81, top: 300, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Latest   </div>
+  <div className="Popular" style={{left: 192, top: 300, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Popular</div>
+  <div className="Premium" style={{left: 303, top: 300, position: 'absolute', color: '#FF0707', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Premium   </div>
+  <div className="Free" style={{left: 424, top: 300, position: 'absolute', color: '#2F5AF1', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Free</div>
+  <div className="Searchbox" style={{width: 364, height: 48, left: 318, top: 223, position: 'absolute', background: 'white', borderRadius: 10}} />
+  <div className="AkarIconsSearch" style={{width: 30, height: 30, paddingTop: 2.50, paddingBottom: 3.75, paddingLeft: 2.50, paddingRight: 3.75, left: 640, top: 231, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+    <div className="Vector" style={{width: 23.75, height: 23.75, border: '3.33px #545454 solid'}}></div>
+  </div>
+  <div style={{left: 291, top: 300, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>|</div>
+  <div className="Vectors" style={{left: 186, top: 51, position: 'absolute', color: 'rgba(99.95, 98.30, 98.30, 0.82)', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Vectors</div>
+  <div className="HighQualityVectorsForPersonalAndCommercialUse" style={{width: 555, left: 234, top: 153, position: 'absolute', color: '#5F5C5C', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>High quality vectors for personal and commercial use</div>
+  <div className="PopularProStockVectors" style={{width: 330, left: 347, top: 111, position: 'absolute', color: 'black', fontSize: 24, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>   Popular Pro Stock Vectors</div>
+  <div className="Psd" style={{left: 416, top: 51, position: 'absolute', color: '#646262', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>PSD</div>
+  <div className="Videos" style={{left: 489, top: 51, position: 'absolute', color: '#646262', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Videos</div>
+  <div className="More" style={{left: 599, top: 51, position: 'absolute', color: 'rgba(99.95, 98.30, 98.30, 0.82)', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>More</div>
+  <div className="Submitbox" style={{width: 140, height: 60, left: 767, top: 42, position: 'absolute', background: 'white', borderRadius: 8, border: '1px black solid'}} />
+  <div className="Photos" style={{left: 306, top: 51, position: 'absolute', color: 'rgba(99.95, 98.30, 98.30, 0.82)', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Photos</div>
+  <div className="Submit" style={{left: 814, top: 53, position: 'absolute', color: '#646262', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Submit</div>
+  <div className="AkarIconsPlus" style={{paddingTop: 0.25, paddingBottom: 14.75, paddingLeft: 14.75, paddingRight: 0.25, left: 764, top: 61, position: 'absolute', justifyContent: 'flex-end', alignItems: 'center', display: 'inline-flex'}}>
+    <div className="Vector" style={{width: 15, height: 15, border: '3.33px #545454 solid'}}></div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Website
