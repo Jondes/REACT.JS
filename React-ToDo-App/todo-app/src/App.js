27e2b1c77816f4a1dc073b60ff2301670e51529e
Johnson Guedes <jondes560@hotmail.com>
2024-06-21 09:49:46 +0100

    React Todo App app.js

--- React-ToDo-App/todo-app/src/App.js
+++ React-ToDo-App/todo-app/src/App.js
@@ -1,11 +1,43 @@
+import { useState } from 'react';
 import './App.css';
+import CreateArea from './components/CreateArea';
+import Header from './components/Header';
+import Note from './components/Note';
+import Footer from './components/Footer';
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [ ...prevNotes, newNote];
+    });
+  }
+function deleteNote(id){
+   setNotes(prevNotes => {
+    return prevNotes.filter((noteItem, index) =>{
+      return index!==id;
+    });
+   });
+}
   return (
-    <div className="App">
-      <h1> Welcome to To Do List</h1>
-      
-    </div>
+    <div>
+      <Header />
+      <CreateArea onAdd = {addNote}/>
+      {notes.map((noteItem, index) => {
+        return(
+                <Note
+                key={index}
+                id = {index}
+                title = {noteItem.title}
+                content = {noteItem.content}
+                onDelete={deleteNote}
+                />
+        );
+      })};
+      <Footer/>
+     </div>
   );
 }
 
